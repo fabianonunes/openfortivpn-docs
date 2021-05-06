eb3278817fe448b53108388f0873bd9c9e76c4ba
Fabiano Parente <fabiano@nunes.me>
2021-05-06 07:53:28 -0300

    remove arquivos não utilizados

--- install.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-dpkg --fsys-tarfile SafenetAuthenticationClient-BR-10.0.37-0_amd64.deb | \
-    tar -C teste -xvf - --transform='s|^|usr/|S' --wildcards './lib/libeToken.so*' './lib/libcardos*'
-    
-dpkg --fsys-tarfile SafenetAuthenticationClient-BR-10.0.37-0_amd64.deb | \
-    tar -C teste -xvf - --transform='s|share/eToken|lib/pcsc|S' ./usr/share/eToken/drivers
